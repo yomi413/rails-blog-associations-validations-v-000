4f8e0629a24ecb0f6d7ea1cfccea03fd153dbb8c
Yomaira Escano <yjescano@gmail.com>
2018-03-27 17:26:29 +0000

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_many :posts
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 RailsBlog::Application.routes.draw do
-
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
